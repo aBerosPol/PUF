ae3602015d389fff0339ad20c8bafeef859e4066
aBerosPol <aberospol@pmfst.hr>
2026-04-08 22:42:25 +0200

    Add files via upload

---
+++ gibanjev2.py
@@ -0,0 +1,21 @@
+import numpy as numpy
+from particlev2 import Particle
+
+v0 = 10
+theta = numpy.pi/4
+x0 = 0
+y0 = 0
+
+particle = Particle (v0, theta, x0, y0)
+
+rangeNumeric = particle.range (dt = 0.001)
+
+g = 9.81
+rangeAnalytic = (v0**2 * numpy.sin(2*theta)) / g
+
+if rangeNumeric == rangeAnalytic:
+    print ("Analiticko rjesenje se poklapa s numerickim.")
+else:
+    print ("Analiticko rjesenje se ne poklapa s numerickim.")
+
+print (f"Numericki domet je {rangeNumeric} \n Analiticki domet je {rangeAnalytic} \n Odstupanje iznosi {abs(rangeNumeric - rangeAnalytic)}")
